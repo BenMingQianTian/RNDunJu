bd3a8cf8596999efb6f5744413c428fb8f2a03aa
zhouhonggang <zhouhonggang@roborock.com>
2021-04-23 16:44:48 +0800

    Init Project And Create BlockGrid

---
+++ gong/BlockGrid.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const BlockGrid = ()=>{
+    return <View>
+        <Text>Block</Text>
+    </View>
+};
+
+export default BlockGrid;
---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+// import App from './App';
+import BlockGrid from './gong/BlockGrid'
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => BlockGrid);
